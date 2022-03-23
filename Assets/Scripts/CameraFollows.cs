6534f4f3082f58e5afebfaeae8fa39f89ce08223
Kevin Campa <kevincampa01@gmail.com>
2022-03-22 22:16:36 -0700

    Agregando la animacion del ajolote

--- Assets/Scripts/CameraFollows.cs
+++ Assets/Scripts/CameraFollows.cs
@@ -10,7 +10,7 @@ public class CameraFollows : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y + 2, this.transform.position.z);
 
 
     }
